a7f7049d1e1cacf0ad6a5d0eb0162dab9f20c825
ZabdiBen <montaeze@gmail.com>
2023-09-01 17:00:46 +0300

    Instalado socket.io

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const http = require('http');
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+//   res.send('<h1>Hello world</h1>');
+res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(3000, () => {
